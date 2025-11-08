d6eb53640eb7019d6f154f74162fe73f5ed64b2d
Sunny <dannandphilareonfire@gmail.com>
2025-11-07 23:52:06 -0500

    GameManager.cs and Game Scene: Rachel Task 1
    Added EnemyR

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject enemyOnePrefab;
+    public GameObject enemyRPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyR", 3, 4);
     }
 
     // Update is called once per frame
@@ -23,4 +25,11 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
     }
+
+//Rachel Task 1, Enemy R
+    void CreateEnemyR()
+    {
+        Instantiate(enemyRPrefab, new Vector3(-9f, Random.Range(3f, 5.5f), 0), Quaternion.identity);
+    }
+   
 }
