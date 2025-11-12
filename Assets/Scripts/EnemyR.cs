085c344dde487afc6ae7e1c8a3054c21383d1a6f
Sunny <dannandphilareonfire@gmail.com>
2025-11-12 15:17:58 -0500

    New Files to update to week 3

--- Assets/Scripts/EnemyR.cs
+++ Assets/Scripts/EnemyR.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class EnemyR : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject explosionPrefab;
+    
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
     void Start()
     {
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
-    // Rachel Week 1 Task 2
     void Update()
     {
-        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 1.5f);
-        if (transform.position.x > 9f)
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 3f);
+        if (transform.position.x < -10f)
         {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Player")
+        {
+            whatDidIHit.GetComponent<PlayerController>().LoseALife();
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        } else if(whatDidIHit.tag == "Weapons")
+        {
+            Destroy(whatDidIHit.gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            gameManager.AddScore(5);
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,160 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject playerPrefab;
     public GameObject enemyOnePrefab;
     public GameObject enemyRPrefab;
     public GameObject enemyKPrefab;
+    public GameObject cloudPrefab;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject powerupPrefab;
+    public GameObject audioPlayer;
+
+    public AudioClip powerupSound;
+    public AudioClip powerdownSound;
+
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI powerupText;
+    
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
+    public int cloudMove;
+
+    private bool gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        cloudMove = 1;
+        gameOver = false;
+        AddScore(0);
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
         InvokeRepeating("CreateEnemyOne", 1, 2);
         InvokeRepeating("CreateEnemyR", 3, 4);
         InvokeRepeating("CreateEnemyK", 5, 6);
+        StartCoroutine(SpawnPowerup());
+        powerupText.text = "No powerups yet!";
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     void CreateEnemyOne()
     {
-        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) *
+        0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
     }
     // Rachel Week 1 Task 2
     void CreateEnemyR()
     {
-        Instantiate(enemyRPrefab, new Vector3(-9f, Random.Range(3f, 5.5f), 0), Quaternion.identity);
+        Instantiate(enemyRPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) *
+        0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
     }
+
     // Kaitlyn Week 1 Task 3
     void CreateEnemyK()
     {
-        Instantiate(enemyKPrefab, new Vector3(9f, Random.Range(3f, 5.5f), 0), Quaternion.identity);
+        Instantiate(enemyKPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) *
+        0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
     }
+
+    void CreatePowerup()
+    {
+        Instantiate(powerupPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+
+    public void ManagePowerupText(int powerupType)
+    {
+        switch (powerupType)
+        {
+            case 1:
+                powerupText.text = "Speed!";
+                break;
+            case 2:
+                powerupText.text = "Double Weapon!";
+                break;
+            case 3:
+                powerupText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerupText.text = "Shield!";
+                break;
+            default:
+                powerupText.text = "No powerups yet!";
+                break;
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        float spawnTime = Random.Range(3, 5); 
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerup();
+        StartCoroutine(SpawnPowerup());
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerupSound);
+                break;
+            case 2:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerdownSound);
+                break;
+        }
+    }
+
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        restartText.SetActive(true);
+        gameOver = true;
+        CancelInvoke();
+        cloudMove = 0;
+    }
+
 }
